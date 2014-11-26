737effef2d57d3372aed3f14169f5f0e4fdb6bc6
jose <jmlriego@gmail.com>
2014-11-26 18:41:49 +0100

    Tabulaciones corregidas

--- fibonacci.py
+++ fibonacci.py
@@ -7,16 +7,16 @@ to pass arguments to the program at RUNTIME (when you run the program).
 """
 import sys # This library provides system-specific functions.
 times = int( # Force the parameter to be an integer.
-sys.argv[1] # The first argument after the program name.
+	sys.argv[1] # The first argument after the program name.
 )
 # Define a function (fib) which takes one parameter (t).
 def fib(t):
-f = [ 0, 1 ]
-for i in range(len(f),t):
-fn = f[len(f)-1] + f[len(f)-2]
-f.append(fn)
-print f
-fib(times) # Call function fib and pass the value of times as its parameter.
+	f = [ 0, 1 ]
+	for i in range(len(f),t):
+		fn = f[len(f)-1] + f[len(f)-2]
+		f.append(fn)
+	print f
+	fib(times) # Call function fib and pass the value of times as its parameter.
 """
 Result:
 $ ~: fibonacci.py 12
