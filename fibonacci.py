291377d894825ffc0e4a153954b8fb5782f53cc9
Jose <jmlriego@gmail.com>
2015-01-19 16:30:26 +0100

    Revert "Eliminados comentarios"
    This reverts commit 9174f4ad62f919176e2da9ea361af3b6087535ea.
    
    Conflicts:
    	fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,32 @@
-#Nada por aqui
+"""Fibonacci.py"""
+#!/usr/bin/python
+"""
+If someone using our program wants to get 13 numbers in the Fibonacci sequence
+(or 10 or 100 or any other number), they have to edit our program by hand.
+That's a hassle, so Python has LIBRARIES (collections of tools) that allow you
+to pass arguments to the program at RUNTIME (when you run the program).
+"""
+import sys # This library provides system-specific functions.
+times = int( # Force the parameter to be an integer.
+sys.argv[1] # The first argument after the program name.
+)
+# Define a function (fib) which takes one parameter (t).
+def fib(t):
+f = [ 0, 1 ]
+for i in range(len(f),t):
+fn = f[len(f)-1] + f[len(f)-2]
+f.append(fn)
+print f
+fib(times) # Call function fib and pass the value of times as its parameter.
+"""
+Result:
+$ ~: fibonacci.py 12
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+$ ~: fibonacci.py 15
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+NOTE: If you use anything other than a number as an argument, the program will
+crash when you try to run it. Can you find a way to make sure the argument is
+a number?
+Este comentario podría traducirse
+"""
+
